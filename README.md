a6b7e62ba5139554d5d553c56da4d1528813d52e
Saverio <saveriocutrupi@users.noreply.github.com>
2021-03-23 11:25:59 +0000

    updated data_types&operators.py

--- README.md
+++ README.md
@@ -23,8 +23,10 @@
 ### Strings and Casting
 
 - Indexing in Python starts with 0
-- ```H e l l o   W o r l d  !
-0 1 2 3 4 5 6 7 8 9 10 11```
+- ```
+H e l l o   W o r l d  !
+0 1 2 3 4 5 6 7 8 9 10 11
+```
 
 - Reverse indexing
 - `print(strin[0:-1])`, a negative integer starts to cound from the end of the string
---
+++ data_types&operatos.py
@@ -0,0 +1,33 @@
+""" The Data types in action """
+
+a = 30
+b = 15
+c = 5.5
+
+# Arithmetic Operators:
+# print(a + b)
+# print(a - b)
+# print(a / b)
+# print(a * b)
+# print(a + C)
+
+
+# Comparison Operators:
+# print(a > b)  # True
+# print(a < b)  # False
+
+
+# Boolean methods
+greeting = "hello world!"
+
+# isalpha() - contains only alphabetical chars
+print(greeting.isalpha())
+
+# islower() - contains only lower case letters
+print(greeting.islower())
+
+# endswith() - ends with a specified substring
+print(greeting.endswith("!"))
+
+# startswith() - starts with as specified substring
+print(greeting.startswith("h"))
